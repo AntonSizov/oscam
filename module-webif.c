AntonSizov/oscam

commit 1256f6d110f9259bffec86f0c56dd5aa0cdb703a

webif: Show picon image only when the picon exists.
It turns out that showing empty image in the place of non existing
picon annoys users (who would've known :) With this changeset the
code checks whether picon can be found and only if it exists then
shows it, otherwise channel name is shown instead.

This is another fix for ticket #2672.

git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@7735 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-webif.c
@@ -2210,6 +2210,12 @@ static void clear_system_stats(void) {
 	cacheex_clear_client_stats(first_client);
 }
 
+static bool picon_exists(uint16_t caid, uint16_t srvid) {
+	char picon_name[16], path[255];
+	snprintf(picon_name, sizeof(picon_name) - 1, "IC_%04X_%04X", caid, srvid);
+	return strlen(tpl_getTplPath(picon_name, cfg.http_tpl, path, sizeof(path) - 1)) && file_exists(path);
+}
+
 static char *send_oscam_user_config(struct templatevars *vars, struct uriparams *params, int32_t apicall) {
 	struct s_auth *account;
 	struct s_client *cl;
@@ -2459,8 +2465,7 @@ static char *send_oscam_user_config(struct templatevars *vars, struct uriparams 
 				if(latestclient){
 					tpl_printf(vars, TPLADD, "CLIENTCAID", "%04X", latestclient->last_caid);
 					tpl_printf(vars, TPLADD, "CLIENTSRVID", "%04X", latestclient->last_srvid);
-
-					if (cfg.http_showpicons) {
+					if (cfg.http_showpicons && picon_exists(latestclient->last_caid, latestclient->last_srvid)) {
 						tpl_printf(vars, TPLADD, "LASTCHANNEL",
 							"<img class=\"clientpicon\" src=\"image?i=IC_%04X_%04X\" alt=\"%s\" title=\"%s\">",
 							latestclient->last_caid, latestclient->last_srvid, lastchan, lastchan);

commit 0519244aba066fc52150856cc29aa8c62c29cd9d

webif: Fix oscam crash caused by commit r7735.
Commit r7735 introduced checks if picons exist but that opened another
bug that allowed tpl_getFilePathInSubdir() to be called with NULL
parameter as path. Which of course causes segfault.

This fixes ticket #2873.

module-webif.c
@@ -2212,6 +2212,8 @@ static void clear_system_stats(void) {
 
 static bool picon_exists(uint16_t caid, uint16_t srvid) {
 	char picon_name[16], path[255];
+	if (!cfg.http_tpl)
+		return false;
 	snprintf(picon_name, sizeof(picon_name) - 1, "IC_%04X_%04X", caid, srvid);
 	return strlen(tpl_getTplPath(picon_name, cfg.http_tpl, path, sizeof(path) - 1)) && file_exists(path);
 }